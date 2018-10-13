5c9ba73131a3b606c11a265af34f503d8ce9c12e
leshghi <43756305+leshghi@users.noreply.github.com>
2018-10-13 13:48:02 -0400

    Add files via upload

---
+++ schedulepage.js
@@ -0,0 +1,32 @@
+
+document.getElementById("AddEvent").addEventListener("click", addEvent);
+document.getElementById("DeleteEvent").addEventListener("click", deleteEvent);
+document.getElementById("AddTask").addEventListener("click", addTask);
+document.getElementById("DeleteTask").addEventListener("click", deleteTask);
+
+
+
+function addEvent(){
+        //takes days and times of event and adds to calendar
+        //also blocks time out of open time
+
+}
+
+function deleteEvent(){
+    //takes days and times of event to delete and deletes from calendar
+    //adds time to open time
+
+}
+
+function addTask(){
+    //takes name and length of task to complete
+    //finds place to add to calendar
+    //removes time from open time
+
+}
+
+function deleteTask(){
+    //takes name of task to delete
+    //deletes from calendar
+    //adds time to open time
+}
